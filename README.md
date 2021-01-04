89f864b259c710017bf4cfa143385f877e5671bf
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-01-04 09:33:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Drogue IoT RAK811
+
+A network driver for a RAK811 attached via a UART.
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![no_std]
+
+// pub use adapter::initialize;
+
+pub const BUFFER_LEN: usize = 2048;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
